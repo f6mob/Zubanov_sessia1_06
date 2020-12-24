65d2129b13cdd0a9fdc8f57b2dbcdf71ec0815e4
user <user@C0907.local>
2020-12-24 13:38:08 +0400

    Initial commit

---
+++ app/src/main/java/com/example/myprojectz/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myprojectz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.service.autofill.OnClickAction;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+}
